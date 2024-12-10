c3cddedb487bbc0212adcacd710e698ed23c4693
itsAPK <apkvandagadde2@gmail.com>
2024-12-10 23:22:57 +0530

    Iniital

--- src/app/_components/documents/columns.tsx
+++ src/app/_components/documents/columns.tsx
@@ -1,53 +1,49 @@
-import { Button } from "@/components/ui/button";
-import { Template } from "@/schema/template";
-import { ColumnDef } from "@tanstack/react-table";
+import { Button } from '@/components/ui/button';
+import { Template } from '@/schema/template';
+import { ColumnDef } from '@tanstack/react-table';
 import { RiDeleteBin6Fill, RiDownload2Fill } from '@remixicon/react';
-import { useRouter } from "next/navigation";
-import { DeleteTemplate } from "./delete";
+import { useRouter } from 'next/navigation';
+import { DeleteTemplate } from './delete';
+import { getCookie } from 'cookies-next';
 export const templateColumns = (): ColumnDef<Template>[] => {
-    return [
-      {
-        accessorKey: "name",
-        header: ({ column }) => (
-          <div className="text-xs flex justify-center  font-medium text-center ">
-           Template Title
-          </div>
-        ),
+  return [
+    {
+      accessorKey: 'name',
+      header: ({ column }) => (
+        <div className="flex justify-center text-center text-xs font-medium">Template Title</div>
+      ),
+    },
+    {
+      accessorKey: 'author',
+      header: ({ column }) => (
+        <div className="flex justify-center text-center text-xs font-medium">Author</div>
+      ),
+    },
+    {
+      accessorKey: 'created_at',
+      header: ({ column }) => (
+        <div className="flex justify-center text-center text-xs font-medium">Date of Upload</div>
+      ),
+      cell: ({ row }) => {
+        return new Date(row.original.created_at).toDateString();
       },
-      {
-          accessorKey: "author",
-          header: ({ column }) => (
-            <div className="text-xs flex justify-center font-medium text-center ">
-              Author
-            </div>
-          ),
-        },
-        {
-          accessorKey: "created_at",
-          header: ({ column }) => (
-            <div className="text-xs flex  justify-center font-medium text-center ">
-              Date of Upload
-            </div>
-          ),
-          cell: ({ row }) => {
-            return new Date(row.original.created_at).toDateString();
-          }
-        },
-        
-   
-      {
-        id: "actions",
-        cell: function Cell({ row }) {
-        
-          return (
-            <div className="flex justify-center space-x-2 pl-2">
-               <Button variant="edit" size={"sm"} className="gap-2 text-xs">
-         <RiDownload2Fill className="mr-2 h-4 w-4" /> Download
-               </Button>
-               <DeleteTemplate id={row.original._id} />
-            </div>
-          );
-        },
-      }, 
-    ];
-  };
+    },
+
+    {
+      id: 'actions',
+      cell: function Cell({ row }) {
+        const role = getCookie('ci-portal.role');
+        return (
+          <div className="flex justify-center space-x-2 pl-2">
+            <Button variant="edit" size={'sm'} className="gap-2 text-xs">
+              <RiDownload2Fill className="mr-2 h-4 w-4" /> Download
+            </Button>
+            {role === 'admin' && (
+              <DeleteTemplate id={row.original._id} />
+            )}
+          </div>
+        );
+      },
+    },
+  ];
+};
